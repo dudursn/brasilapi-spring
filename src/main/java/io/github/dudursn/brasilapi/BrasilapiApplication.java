89eb4cc579dc019930c3ecdf7db62f973de8d186
Eduardo Nascimento <roger.rsn@outlook.com>
2021-07-27 13:28:39 -0300

    configuracao para gerar war e deploy tomcat

--- src/main/java/io/github/dudursn/brasilapi/BrasilapiApplication.java
+++ src/main/java/io/github/dudursn/brasilapi/BrasilapiApplication.java
@@ -2,12 +2,20 @@ package io.github.dudursn.brasilapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BrasilapiApplication {
+public class BrasilapiApplication extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) {
         SpringApplication.run(BrasilapiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(BrasilapiApplication.class);
     }
 
 }
